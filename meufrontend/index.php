cdfbae0a106f5d1da3bffa5afd3d1174bd88568e
wagnermarques <wagnerdocri@gmail.com>
2026-03-26 19:47:07 -0300

    added

--- meufrontend/index.php
+++ meufrontend/index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt-br">
- 
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VM</title>
+    <title>Document</title>
 </head>
- 
 <body>
-    <?php echo "<h1>joia</h1>" ?>
+    <h1>Meu HTML</h1>
+    <?php 
+        $nome="Claude Shanon";
+        echo "<h2>Olá $nome !!!</h2>" 
+    ?>
 </body>
- 
 </html>
